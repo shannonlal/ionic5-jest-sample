62018cfbbce4d46e4e2bec31d9a253d9f8bfde18
Shannon Lal <shannonlal@hotmail.com>
2020-09-26 22:52:11 -0400

    Get the Service and http into page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExchangeRates } from './models';
+import {ExhangeRateService} from './service/exchange-rate.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor( private exchangeRateService: ExhangeRateService) {
+    this.getExchangeRates();
+  }
+
+  public exchangeRate: ExchangeRates;
+
+  public rates: {key: string; value: string}[] = [];
+
+  ngOnInit(): void {
+  }
+
+  private getExchangeRates() {
+
+    this.exchangeRateService.getExchangeRates().subscribe( ( exchange: ExchangeRates ) => {
+      this.exchangeRate = exchange;
+
+      const rateKeys = Object.keys(this.exchangeRate.rates);
+
+      rateKeys.map( key => {
+        this.rates.push({key, value: this.exchangeRate.rates[key]});
+      });
+
+      console.log( 'rate keys', this.rates);
+
+    });
+  }
 
 }
